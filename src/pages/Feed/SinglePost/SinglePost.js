83b59ff8ae91e1a7c797d8d082ab02583332ec79
Nirmalya Nayak <n.nayak263@gmail.com>
2022-12-22 13:43:14 +0530

    showing image at the front-end

--- src/pages/Feed/SinglePost/SinglePost.js
+++ src/pages/Feed/SinglePost/SinglePost.js
@@ -25,7 +25,7 @@ class SinglePost extends Component {
         this.setState({
           title: resData.post.title,
           author: resData.post.creator.name,
-          image: `http://localhost:8080/feed/post/${resData.post.imageUrl}`,
+          image: `http://localhost:8080/images/${resData.post.imageUrl}`,
           date: new Date(resData.post.createdAt).toLocaleDateString('en-US'),
           content: resData.post.content,
         })
