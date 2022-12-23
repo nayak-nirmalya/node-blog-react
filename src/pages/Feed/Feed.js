45b22b24f1bc7ea856d35b28923ecf3167871518
Nirmalya Nayak <n.nayak263@gmail.com>
2022-12-23 13:01:36 +0530

    listen for post event

--- src/pages/Feed/Feed.js
+++ src/pages/Feed/Feed.js
@@ -40,7 +40,28 @@ class Feed extends Component {
       .catch(this.catchError)
 
     this.loadPosts()
-    openSocket('http://localhost:8080')
+    const socket = openSocket('http://localhost:8080')
+    socket.on('posts', (data) => {
+      if (data.action === 'create') {
+        this.addPost(data.post)
+      }
+    })
+  }
+
+  addPost = (post) => {
+    this.setState((prevState) => {
+      const updatedPosts = [...prevState.posts]
+      if (prevState.postPage === 1) {
+        if (prevState.posts.length >= 2) {
+          updatedPosts.pop()
+        }
+        updatedPosts.unshift(post)
+      }
+      return {
+        posts: updatedPosts,
+        totalPosts: prevState.totalPosts + 1,
+      }
+    })
   }
 
   loadPosts = (direction) => {
@@ -166,9 +187,10 @@ class Feed extends Component {
               (p) => p._id === prevState.editPost._id,
             )
             updatedPosts[postIndex] = post
-          } else if (prevState.posts.length < 2) {
-            updatedPosts = prevState.posts.concat(post)
           }
+          // else if (prevState.posts.length < 2) {
+          //   updatedPosts = prevState.posts.concat(post)
+          // }
           return {
             posts: updatedPosts,
             isEditing: false,
